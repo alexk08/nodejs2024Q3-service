9ada3ea2c2e6860274fb8c5779fc12c16ee77d1e
Aleksandr Krasilnikov <alexander.krasilni@aliexpress.ru>
2024-11-11 23:10:25 +0300

    init develop

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // init
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
 }
